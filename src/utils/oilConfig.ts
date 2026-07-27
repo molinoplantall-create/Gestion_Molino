be84f52a705319d70c086b8b72468b658ce498f5
molinoplantall-create <molinoplantall@gmail.com>
2026-07-27 10:26:49 -0500

    se aumento en umbral a 1000 horas

--- src/utils/oilConfig.ts
+++ src/utils/oilConfig.ts
@@ -1,4 +1,4 @@
 export const getMaxOilHours = (millName: string): number => {
   const isSmallMill = millName === 'Molino I' || millName === 'Molino II' || millName === 'MOLINO I' || millName === 'MOLINO II';
-  return isSmallMill ? 100 : 500;
+  return isSmallMill ? 100 : 1000;
 };
